ca8be64c9874b79adf42494c07d2a36c8f7f7db8
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-08 14:11:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <ListEmployes/>
+</template>
+
+<script setup>
+import ListEmployes from './components/ListEmployes.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListEmployes.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <ol>
+      <li>Employe 1</li>
+      <li>Employe 2</li>
+      <li>Employe 3</li>
+      <li>Employe 4</li>
+      </ol>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  div{
+    display: flex;
+    justify-content: center;
+    width: 600px;
+    margin: 0 20%;
+    background-color: rgb(236, 226, 177);
+  }
+  ol {
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+  }
+  li {
+    font-family: sans-serif;
+    font-size: 1.5em;
+    color: black;
+  }
+  li:hover {
+    background-color: rgb(88, 42, 48);
+    color: white;
+  }
+  </style>
+  
